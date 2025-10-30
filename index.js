40c1d89431a9f1d06b0cd1a2ba70de0695d1ad58
Fur1782 <fur1782@gmail.com>
2025-10-30 17:08:12 +0100

    configuration for dev

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ export const createApp = ({ quizModel, classRoomModel }) => {
 
     const PORT = process.env.PORT ?? 3000;
 
-    server.listen(PORT, () => {
+    server.listen(PORT, '0.0.0.0', () => {
       console.log(`server listening on port http://localhost:${PORT}`)
     })
 }
